22b439227608a11d13592fc5412a45fb003b56df
taylor <taylor@taylordigital.com>
2020-06-02 12:24:45 -0700

    added basic template files

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 app_name = "tagtable"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.dashboard, name="dashboard"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "frontend/index.html")
+def dashboard(request):
+    return render(request, "frontend/dashboard.html")
