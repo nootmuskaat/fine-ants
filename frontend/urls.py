07097db75b1a9bccc30c843a3537f7f41662f930
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2020-06-03 13:07:45 +0200

    Added urls and views for frontend templates

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,10 @@ from . import views
 
 app_name = "tagtable"
 urlpatterns = [
+    path("accounts", views.accounts, name="accounts"),
+    path("categories", views.categories, name="categories"),
+    path("import", views.import_file, name="import"),
+    path("profile", views.profile, name="profile"),
+    path("reports", views.reports, name="reports"),
     path("", views.dashboard, name="dashboard"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "frontend/dashboard.html")
+
+def accounts(request):
+    return render(request, "frontend/accounts.html")
+
+def categories(request):
+    return render(request, "frontend/categories.html")
+
+def import_file(request):
+    return render(request, "frontend/import.html")
+
+def profile(request):
+    return render(request, "frontend/profile.html")
+
+def reports(request):
+    return render(request, "frontend/reports.html")
