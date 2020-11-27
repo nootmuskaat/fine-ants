8e0d84df04e13571ed1a55cb172e1629a26b5335
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2020-11-27 17:23:18 +0100

    Define basic backend models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import Group, User
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    """Such as USD, EUR, GBP, etc"""
+
+    code = models.CharField(max_length=3)
+    precision = models.PositiveSmallIntegerField(default=2)
+
+
+class Account(models.Model):
+    """Generally a bank account"""
+
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name="accounts")
+    name = models.CharField(max_length=255)
+    identifier = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned")
+    group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name="account")
+
+
+class Category(models.Model):
+    """A transaction category"""
+
+    name = models.CharField(max_length=255)
+
+
+class SubCategory(models.Model):
+    """A transaction sub-category"""
+
+    parent = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="children")
+    name = models.CharField(max_length=255)
+
+
+class Transaction(models.Model):
+    """A financial transaction"""
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="transactions")
+    category = models.ForeignKey(
+        SubCategory, on_delete=models.CASCADE, related_name="transactions"
+    )
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    hash = models.CharField(max_length=255)
