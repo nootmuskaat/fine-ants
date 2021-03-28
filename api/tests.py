e26f7b5aabdd8d86b65b26e1ac158d961004f251
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2021-03-28 17:46:00 +0200

    Added tests for categories endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+from api.models import Category, SubCategory
+
+JSON = "application/json"
+
+
+class CategoriesTestCase(TestCase):
+    def test_create_and_fetch_categories(self):
+        c = Client()
+        endpoint = "/api/categories"
+        categories = {
+            "groceries": ["general", "alcohol"],
+            "going-out": ["restaurants", "drinks"],
+        }
+        for parent, children in categories.items():
+            for child in children:
+                data = {"parent": parent, "name": child}
+                c.post(endpoint, data=data, content_type=JSON)
+        response = c.get(endpoint)
+        self.assertEqual(response.json(), categories)
