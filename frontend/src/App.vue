f5e8c6b608a2e2e6b4e30ff19ab4b9a52bf53886
taylor <taylor@taylordigital.com>
2022-04-18 18:12:53 -0700

    added apple and intelji IDE related gitignore entries, added in the bones of the frontend vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="layout">
+    <router-view />
+  </component>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+const defaultLayout = "default";
+
+const { currentRoute } = useRouter();
+
+const layout = computed(
+  () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
+);
+</script>
